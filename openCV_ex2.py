c2906e2ebdefad758d46d8dd6682d104dde9f102
mdrn <ss2013105@yandex.ru>
2025-05-28 17:20:09 +0700

    Update openCV_ex2.py

--- openCV_ex2.py
+++ openCV_ex2.py
@@ -14,9 +14,9 @@ img2 = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)  # convert from LAB to BGR
 
 image = cv2.threshold(cv2.GaussianBlur(cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY), (5, 5), 0), 110, 255, cv2.THRESH_BINARY)[1]
 
-contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-image = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+contours, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
 
-cv2.imshow('Image', image)
+cv2.imshow('Image', img)
 cv2.waitKey(3000)
 cv2.destroyAllWindows()
