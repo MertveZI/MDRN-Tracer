f3d4f2ea4d719fe94231ea75e5a53a0ac772ab58
MertveZI <ss2013105@yandex.ru>
2025-06-15 16:17:01 +0700

    new tracker

--- openCV_ex.py
+++ openCV_ex.py
@@ -1,59 +1,53 @@
 import cv2
+import glob
 
+#Формирование списка с
 
-def imageDrawContours(img):
-    '''Возвращает изображение с обрезанными углами и нарисованными контурами и контуры'''
-    # CLAHE (Contrast Limited Adaptive Histogram Equalization)
-    clahe = cv2.createCLAHE(clipLimit=3., tileGridSize=(9,9))
-    lab = cv2.cvtColor(img[20:-20, 10:-20], cv2.COLOR_BGR2LAB)  # convert from BGR to LAB color space
-    l, a, b = cv2.split(lab)  # split on 3 different channels
-    lab = cv2.merge((clahe.apply(l),a,b))  # merge channels
-    img = cv2.threshold(cv2.GaussianBlur(cv2.cvtColor(cv2.cvtColor(lab, cv2.COLOR_LAB2BGR), cv2.COLOR_BGR2GRAY), (5, 5), 0), 110, 255, cv2.THRESH_BINARY)[1]
-    contours = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0][:-15]
-    cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
+path = 'voskres.mp4'
+# Инициализация трекера и видеопотока
+tracker = cv2.TrackerKCF.create()
+video = cv2.VideoCapture(path)
+
+# Чтение первого кадра
+ret, frame = video.read()
+if not ret:
+    print("Ошибка чтения камеры!")
+    exit()
+
+# Выбор объекта для трекинга
+bbox = cv2.selectROI("Select Object", frame, False)
+cv2.destroyWindow("Select Object")
+
+# Инициализация трекера
+tracker.init(frame, bbox)
+
+while True:
+    # Чтение кадра
+    ret, frame = video.read()
+    if not ret:
+        break
     
-    return [img, contours]
-
-def findPosition(cnts, pos):
-    '''Находит позицию контура, включающего точку'''
-    for cnt in cnts:
-        (x,y),radius = cv2.minEnclosingCircle(cnt)
-        if x - radius < pos[0] and x + radius > pos[0] and y - radius < pos[1] and y + radius > pos[1]:
-            return [(x,y), radius]
-    raise Exception('BadPosition!')
-
-def findPair(img, pos, radius):
-    x1 = (pos[0] - radius*3)
-    x2 = (pos[0] + radius*3)
-    y1 = (pos[1] - radius*3)
-    y2 = (pos[1] + radius*3)
-    sector = img[x1:x2, y1:y2] #Выделяем область поиска
-    img, contours = imageDrawContours(sector)
-    new_pos, new_radius = findPosition(contours, pos)
-    new_pos[0] += pos[0] - radius*3
-    new_pos[1] += pos[1] - radius*3
-    if new_radius >= radius * 2: #Если частицы 'слиплись'
-        new_pos[0] -= (radius + new_radius) * 0.71
-        new_pos[1] -= (radius + new_radius) * 0.71
-    return [new_pos, new_radius]
-
-
-Trace = []
-
-prefix = 'dataset\snap'
-img = cv2.imread('dataset\snap0001.jpg',1)
-img, contours = imageDrawContours(img)
-pos, radius = findPosition(contours,[300,300])
-Trace.append(pos)
-
-for i in range(8):
-    path = prefix + '000' + str(2 + i) + '.jpg'
-    img = cv2.imread(path ,1)
-    img, contours = imageDrawContours(img)
-    pos, radius = findPair(contours, pos, radius)
-    cv2.imshow('Image', img)
-    cv2.waitKey(10)
-    cv2.destroyAllWindows()
+    # Обновление трекера
+    success, bbox = tracker.update(frame)
     
-print(Trace)
-print(radius)
+    # Отрисовка прямоугольника при успешном трекинге
+    if success:
+        x, y, w, h = [int(v) for v in bbox]
+        p1 = (x, y)
+        p2 = (x + w, y + h)
+        cv2.rectangle(frame, p1, p2, (0, 0, 255), 2, 1)
+        
+        # Вывод координат (опционально)
+        print(f"p1: {p1}, p2: {p2}")
+    
+    # Отображение результата
+    cv2.imshow("Tracking", frame)
+    
+    # Выход по ESC
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+# Освобождение ресурсов
+video.release()
+cv2.destroyAllWindows()
--- openCV_ex2.py
+++ openCV_ex2.py
@@ -2,55 +2,59 @@ import cv2
 
 
 def imageDrawContours(img):
-    '''Возвращает изображение с обрезанными углами и нарисованными контурами и контуры'''
+    '''Возвращает изображение с контурами и контуры'''
     # CLAHE (Contrast Limited Adaptive Histogram Equalization)
     clahe = cv2.createCLAHE(clipLimit=3., tileGridSize=(9,9))
-    lab = cv2.cvtColor(img[20:-20, 10:-20], cv2.COLOR_BGR2LAB)  # convert from BGR to LAB color space
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)  # convert from BGR to LAB color space
     l, a, b = cv2.split(lab)  # split on 3 different channels
     lab = cv2.merge((clahe.apply(l),a,b))  # merge channels
-    img = cv2.threshold(cv2.GaussianBlur(cv2.cvtColor(cv2.cvtColor(lab, cv2.COLOR_LAB2BGR), cv2.COLOR_BGR2GRAY), (5, 5), 0), 110, 255, cv2.THRESH_BINARY)[1]
-    contours = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0][:-15]
+    img2 = cv2.threshold(cv2.GaussianBlur(cv2.cvtColor(cv2.cvtColor(lab, cv2.COLOR_LAB2BGR), cv2.COLOR_BGR2GRAY), (5, 5), 0), 110, 255, cv2.THRESH_BINARY)[1]
+    contours = cv2.findContours(img2, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0][:-1]
     cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
     
     return [img, contours]
 
-def findPosition(cnts, pos):
+def findPosition(cnts, old_pos):
     '''Находит позицию контура, включающего точку'''
     for cnt in cnts:
         (x,y),radius = cv2.minEnclosingCircle(cnt)
-        if x - radius < pos[0] and x + radius > pos[0] and y - radius < pos[1] and y + radius > pos[1]:
-            return [(x,y), radius]
+        print(x,y,radius)
+        if x - radius < old_pos[0] and x + radius > old_pos[0] and y - radius < old_pos[1] and y + radius > old_pos[1]:
+            return [[x,y], radius]
+    print(old_pos)
     raise Exception('BadPosition!')
 
 def findPair(img, pos, radius):
-    x1 = (pos[0] - radius*3)
-    x2 = (pos[0] + radius*3)
-    y1 = (pos[1] - radius*3)
-    y2 = (pos[1] + radius*3)
+    x1 = int(pos[0] - radius*6)
+    x2 = int(pos[0] + radius*6)
+    y1 = int(pos[1] - radius*6)
+    y2 = int(pos[1] + radius*6)
     sector = img[x1:x2, y1:y2] #Выделяем область поиска
     img, contours = imageDrawContours(sector)
-    new_pos, new_radius = findPosition(contours, pos)
+    cv2.imshow('Image', img)
+    cv2.waitKey(2000)
+    cv2.destroyAllWindows()
+    new_pos, new_radius = findPosition(contours, [radius*6, radius*6])
     new_pos[0] += pos[0] - radius*3
     new_pos[1] += pos[1] - radius*3
     if new_radius >= radius * 2: #Если частицы 'слиплись'
         new_pos[0] -= (radius + new_radius) * 0.71
         new_pos[1] -= (radius + new_radius) * 0.71
-    return [new_pos, new_radius]
+    return [img, new_pos, new_radius]
 
 
 Trace = []
 
-prefix = 'dataset\snap'
-img = cv2.imread('dataset\snap0001.jpg',1)
+prefix = 'dataset/snap000'
+img = cv2.imread('dataset/snap0001.jpg',1)[20:-20, 20:-20]
 img, contours = imageDrawContours(img)
 pos, radius = findPosition(contours,[300,300])
 Trace.append(pos)
 
 for i in range(8):
-    path = prefix + '000' + str(2 + i) + '.jpg'
-    img = cv2.imread(path ,1)
-    img, contours = imageDrawContours(img)
-    pos, radius = findPair(contours, pos, radius)
+    path = prefix + str(2 + i) + '.jpg'
+    img = cv2.imread(path)[20:-20, 20:-20]
+    img, pos, radius = findPair(img, pos, radius)
     cv2.imshow('Image', img)
     cv2.waitKey(10)
     cv2.destroyAllWindows()
