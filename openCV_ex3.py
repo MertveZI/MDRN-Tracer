1d8cbcff8d8f109312652fd114b64fe2c6063c67
MertveZI <ss2013105@yandex.ru>
2025-06-15 23:26:21 +0700

    added dataset3 and restructed

--- openCV_ex3.py
+++ openCV_ex3.py
@@ -1,38 +1,34 @@
 import cv2
 import glob
 
-#Формирование списка с кадрами
-pathname = 'C:/Users/mdrn/Documents/GitHub/MDRN-Tracer/dataset2/*.jpg'
-snaps = glob.glob(pathname)
-
-
-tracker = cv2.TrackerCSRT.create()
-frame = cv2.imread(snaps[0],1)[20:-20, 20:-20]
-
-# Выбор объекта для трекинга
-bbox = cv2.selectROI("Select Object", frame, False)
-cv2.destroyWindow("Select Object")
-
-
-
-tracker.init(frame, bbox)
-i=1
-length = len(snaps) - 1
-while True:
-    frame = cv2.imread(snaps[i],1)[20:-20, 20:-20]
-    i+=1
-    
-    success, bbox = tracker.update(frame)
-    
+def find_pair(frame, tracker):
+    success, roi = tracker.update(frame)
     # Отрисовка прямоугольника
     if success:
-        x, y, w, h = [int(v) for v in bbox]
+        x, y, w, h = [int(v) for v in roi]
         p1 = (x, y)
         p2 = (x + w, y + h)
         cv2.rectangle(frame, p1, p2, (0, 0, 255), 2, 1)
-        
-        # Вывод координат
-        print(f"p1: {p1}, p2: {p2}")
+    return(p1,p2)
+#Формирование списка с кадрами
+pathname = 'C:/Users/mdrn/Documents/GitHub/MDRN-Tracer/dataset2/*.jpg'
+snaps = glob.glob(pathname)
+frame = cv2.imread(snaps[0],1)[20:-20, 20:-20]
+
+# Инициализация трекера и области интереса
+tracker = cv2.TrackerCSRT.create()
+roi = cv2.selectROI("Select Object", frame, False)
+cv2.destroyWindow("Select Object")
+tracker.init(frame, roi)
+
+#Трекинг
+length = len(snaps) - 2
+i=0
+while True:
+    i += 1
+    frame = cv2.imread(snaps[i],1)[20:-20, 20:-20]
+    p1,p2 = find_pair(frame,tracker)
+    print(f"p1: {p1}, p2: {p2}")
     
     cv2.imshow("Tracking", frame)
     
